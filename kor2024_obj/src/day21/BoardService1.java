4d3c05c8c7310069f38eff4efcaeeb67bd3417db
dynam <dynamicpurs@gmail.com>
2024-11-12 20:26:52 +0900

    commit

---
+++ kor2024_obj/src/day21/BoardService1.java
@@ -0,0 +1,11 @@
+/*
+    BoardService1
+        - 내용 과 작성자로 구성된 게시물 3개 까지 저장하는 서비스 구축
+        - 조건 : main 1개 와 변수 6개와 반복문 사용
+        - 구축 : 게시물 쓰기와 게시물 출력 기능 구현
+ */
+package day21;
+
+public class BoardService1 {
+
+} // cls end
